2dd77836ea630f2ccb5edd65adeb9be6649fc07f
BİLAL ADIYAMAN <99627537+REFLESHONEONE@users.noreply.github.com>
2022-07-14 16:47:22 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+var ltoo = document.getElementById("l-too");
+var rtoo = document.getElementById("r-too");
+var btnx = document.getElementById("btn");
+function full() {
+  ltoo.style.left = "-500px";
+  rtoo.style.left = "70px";
+  btnx.style.left = "150px";
+}
+function fulla() {
+  ltoo.style.left = "70px";
+  rtoo.style.left = "500px";
+  btnx.style.left = "0";
+}
